d197e50964a6582522a6c8e9b83e0784f3d3bfee
Art Beckman <aabeckman@yahoo.com>
2018-05-21 21:35:06 -0700

    Assignment 1

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const dishRouter = require('./routes/dishRouter');
+const promotionsRouter = require('./routes/promoRouter');
+const leadersRouter = require('./routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -11,6 +13,8 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 app.use(express.static(__dirname + '/public'));
 
